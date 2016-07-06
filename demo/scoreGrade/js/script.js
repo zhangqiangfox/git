656c4bb8236df2ae1099e96ccaea4c8e6dfa0fe4
zhangqiangfox <zhangqiangfox@users.noreply.github.com>
2016-07-06 15:02:35 +0800

    Update script.js

--- demo/scoreGrade/js/script.js
+++ demo/scoreGrade/js/script.js
@@ -1,5 +1,5 @@
  //js部分;
- //if方式
+ //switch方式
 function level() {//定义函数名;
     var score = document.getElementById("num").value;
     switch (true) {
@@ -30,41 +30,40 @@ function level() {//定义函数名;
 }
 
 
-// switch方式开始
+// if方式开始
 
 
-var score = 100
-switch (true) {
-	case score <= 100 && score >= 90:
-		alert("请重新输入分数！");
-		break;
-	case score < 90 && score >= 80:
-		alert("该生为二等生");
-		break;
-	case score < 80 && score >= 70:
-		alert("该生为三等生");
-		break;
-	case score < 70 && score >= 60:
-		alert("该生为四等生");
-		break;
-	case score < 60 && score >= 50:
-		alert("该生为五等生");
-		break;
-	case score < 50 && score >= 40:
-		alert("该生为六等生");
-		break;
-	case score < 40 && score >= 30:
-		alert("该生为七等生");
-		break;
-	case score < 30 && score >= 20:
-		alert("该生为八等生");
-		break;
-	case score < 20 && score >= 10:
-		alert("该生为久等生");
-		break;
-	case score < 10 && score >= 0:
-		alert("该生为十等生");
-		break;
-}
-// switch方式结束
+var stutent = 90
+if (stutent <= 100 && stutent >= 90) {
+	console.warn(1);
+
+} else if (stutent < 90 && stutent >= 80) {
+	console.warn(2);
+
+} else if (stutent < 80 && stutent >= 70) {
+	console.warn(3);
+
+} else if (stutent < 70 && stutent >= 60) {
+	console.warn(4);
+
+} else if (stutent < 60 && stutent >= 50) {
+	console.warn(5);
+
+} else if (stutent < 50 && stutent >= 40) {
+	console.warn(6);
+
+} else if (stutent < 40 && stutent >= 30) {
+	console.warn(7);
+
+} else if (stutent < 30 && stutent >= 20) {
+	console.warn(8);
+
+} else if (stutent < 20 && stutent >= 10) {
+	console.warn(9);
+
+} else if (stutent < 10 && stutent >= 0) {
+	console.warn(10);
+
+};
+
 //js部分结束;
